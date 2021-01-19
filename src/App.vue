6382fb2b3d377098ddbd8b02149b4a6e2b1c5623
Michał Strzelecki <michasstrzelecki@gmail.com>
2021-01-19 15:36:57 +0100

    add select form to allow users to choose status of the pet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <v-app>
     <v-main>
+      <v-col
+          class="d-flex"
+          cols="12"
+          sm="6"
+      >
+        <v-select
+            v-model="selectedPetStatus"
+            :items="petStatus"
+            hint="Change pet availability"
+            solo
+            return-object
+            persistent-hint
+            @change="onPetStatusChange"
+        ></v-select>
+      </v-col>
       <CardList :items="pets" />
     </v-main>
   </v-app>
@@ -9,7 +24,7 @@
 <script>
 import CardList from "@/components/CardList";
 
-import getPets from "@/api/findPetsByStatus";
+import findPetsByStatus from "@/api/findPetsByStatus";
 
 export default {
   name: 'App',
@@ -19,13 +34,40 @@ export default {
   },
 
   data: () => ({
+    selectedPetStatus: {
+      text: 'Available',
+      value: 'available'
+    },
+    petStatus: [
+      {
+        text: 'Available',
+        value: 'available'
+      },
+      {
+        text: 'Pending',
+        value: 'pending'
+      },
+      {
+        text: 'Sold',
+        value: 'sold'
+      }
+    ],
     pets: []
   }),
 
-  async beforeMount() {
-    const { data } = await getPets()
-    console.log(data[0])
-    this.pets = data
+  beforeMount() {
+    this.getPets()
   },
+
+  methods: {
+    async getPets() {
+      const { data } = await findPetsByStatus(this.selectedPetStatus.value)
+      console.log(data[0])
+      this.pets = data
+    },
+    onPetStatusChange() {
+      this.getPets()
+    }
+  }
 };
 </script>
